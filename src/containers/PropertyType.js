519aa773b0fd5a2da07c73687d871429fda170b0
Claire Marchal <clairemarchal@mbp-de-claire.home>
2019-12-17 13:39:10 +0100

    ECRAN 1 ok

--- src/containers/PropertyType.js
+++ src/containers/PropertyType.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import Card from "../components/Card.js";
 // import React, { useState, useEffect } from "react";
 // import axios from "axios";
 
 const PropertyType = () => {
+  // const [isSelected, setIsSelected] = useState(false);
+
   return (
     <>
       <div className="wrapper">
         <div className="main-title">type de bien</div>
 
         <div className="property-type-box-container">
-          <input
-            type="radio"
-            id="property-type-1"
-            name="property-type"
-            value="maison"
-            // checked
-          ></input>
-          <label for="property-type-1">maison</label>
+          <div className="property-type-box">
+            <input
+              type="radio"
+              id="property-type-1"
+              name="property-type"
+              value="house"
+            >
+              {/* {setIsSelected(true)} */}
+            </input>
+            <label className="property-type-text" for="property-type-1">
+              maison
+            </label>
+          </div>
+          <div className="property-type-box">
+            <input
+              type="radio"
+              id="property-type-2"
+              name="property-type"
+              value="flat"
+            ></input>
+            <label className="property-type-text" for="property-type-1">
+              appartement
+            </label>
+          </div>
         </div>
-        <div>Bienvenue sur la première page 1 "PropertyType"</div>
-        <Link to={"/property_condition"}>
-          <div>Lien vers la page 2 "PropertyCondition"</div>
-        </Link>
+        <div className="footer-nav">
+          <div className="footer-button-left">Précédent</div>
+          <Link className="link" to={"/property_condition"}>
+            <div className="footer-button-right">Suivant</div>
+          </Link>
+        </div>
       </div>
     </>
   );
